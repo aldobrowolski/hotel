9e1204c70918ef68a8e94bc698b98d1259620690
Aleksander Dobrowolski <aldo@grapeup.com>
2021-05-13 20:39:18 +0200

    Dto and entity tests.

--- src/main/java/com/exercise/hotel/configuration/SecurityConfiguration.java
+++ src/main/java/com/exercise/hotel/configuration/SecurityConfiguration.java
@@ -11,5 +11,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
     	http.csrf().disable().authorizeRequests().anyRequest().permitAll();    
         }
-
 }
